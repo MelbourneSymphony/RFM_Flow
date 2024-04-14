3354e66cb922d263591b513299e498c446d74923
Sam Harvey <70475300+Melsuser5@users.noreply.github.com>
2024-04-15 09:58:51 +1000

    updated dashboard

---
+++ sankey.py
@@ -0,0 +1,130 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+
+
+st.set_page_config(layout="centered")
+st.set_option('deprecation.showPyplotGlobalUse', True)
+st.title("MSO RFM Segmentation Flows")
+
+df= pd.read_csv('https://raw.githubusercontent.com/Melsuser5/RFM_FLOW/main/2024_q1_rfm_sankey.csv')
+unique_source_target = list(pd.unique(df[['Source', 'Target']].values.ravel('K')))
+mapping_dict = {k: v for v, k in enumerate(unique_source_target)}
+df['Source'] = df['Source'].map(mapping_dict)
+df['Target'] = df['Target'].map(mapping_dict)
+links_dict = df.to_dict(orient='list')
+color_dict = ['#F94144','#F94144','#749A7F','#749A7F','#58B0D6','#58B0D6','#F9C74F','#F9C74F','#F3722C','#F3722C','#90BE6D','#90BE6D','#577590','#577590','#277DA1','#277DA1','#F94144','#577590','#F9C74F','#58B0D6','#F3722C','#90BE6D','#277DA1','#277DA1','#277DA1']
+
+color_dict_link = df['link_colour'].tolist()
+
+
+
+#First Sankey
+fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 20,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = unique_source_target,
+      color = color_dict,
+    ),
+    link = dict(
+      arrowlen = 15,
+      source = links_dict["Source"],
+      target = links_dict["Target"],
+      value = links_dict["values"],
+      color = color_dict_link,
+
+  ))])
+
+fig.update_layout(title_text="RFM Customer Flow", font_size=10,width=1000, height=600,
+
+  xaxis={
+  'showgrid': False, # thin lines in the background
+  'zeroline': False, # thick line at x=0
+  'visible': False,  # numbers below
+  },
+  yaxis={
+  'showgrid': False, # thin lines in the background
+  'zeroline': False, # thick line at x=0
+  'visible': False,  # numbers below
+  }, plot_bgcolor='rgba(0,0,0,0)')
+
+for x_coordinate, column_name in enumerate(["2023 Quarter 3","2023 Quarter 4","2024 Quarter 1"]):
+    fig.add_annotation(
+          x=x_coordinate,#Plotly recognizes 0-5 to be the x range.
+
+          y=1.075,#y value above 1 means above all nodes
+          xref="x",
+          yref="paper",
+          text=column_name,#Text
+          showarrow=False,
+          font=dict(
+              family="Tahoma",
+              size=12,
+              color="black"
+              ),
+          align="left",
+          )
+st.plotly_chart(fig, use_container_width=True)
+#sankey 2
+df2= pd.read_csv('https://raw.githubusercontent.com/Melsuser5/RFM_FLOW/main/minor_flows_q1.csv')
+unique_source_target2 = list(pd.unique(df2[['Source', 'Target']].values.ravel('K')))
+mapping_dict = {k: v for v, k in enumerate(unique_source_target2)}
+df2['Source'] = df2['Source'].map(mapping_dict)
+df2['Target'] = df2['Target'].map(mapping_dict)
+links_dict2 = df2.to_dict(orient='list')
+
+colours2 =['#58B0D6','#58B0D6','#F9C74F','#F9C74F','#F3722C','#F3722C','#90BE6D','#90BE6D','#58B0D6','#577590','#577590','#F9C74F','#F3722C','#90BE6D']
+link_colours_minor = df2['link_colour'].to_list()
+]
+#Sankey Diagram Code
+fig2 = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = unique_source_target2,
+      color = colours2,
+    ),
+    link = dict(
+      arrowlen = 15,
+      source = links_dict2["Source"],
+      target = links_dict2["Target"],
+      value = links_dict2["values"],
+      color = link_colours_minor,
+
+  ))])
+
+fig2.update_layout(title_text="RFM Customer Minor Flow", font_size=10,width=1000, height=600,
+
+  xaxis={
+  'showgrid': False, # thin lines in the background
+  'zeroline': False, # thick line at x=0
+  'visible': False,  # numbers below
+  },
+  yaxis={
+  'showgrid': False, # thin lines in the background
+  'zeroline': False, # thick line at x=0
+  'visible': False,  # numbers below
+  }, plot_bgcolor='rgba(0,0,0,0)')
+
+for x_coordinate, column_name in enumerate(["2023 Quarter 3","2023 Quarter 4","2024 Quarter 1"]):
+    fig2.add_annotation(
+          x=x_coordinate,#Plotly recognizes 0-5 to be the x range.
+
+          y=1.075,#y value above 1 means above all nodes
+          xref="x",
+          yref="paper",
+          text=column_name,#Text
+          showarrow=False,
+          font=dict(
+              family="Tahoma",
+              size=12,
+              color="black"
+              ),
+          align="left",
+          )
+st.plotly_chart(fig2, use_container_width=True)
+
+st.write("to go back to the main dashboard click [here](https://rfmlabels-t7qm6v74os.streamlit.app/)")
